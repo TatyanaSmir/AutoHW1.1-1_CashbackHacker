bf15b6809ba7b4461ddbab17f464830e0fd5a2e9
Tatyana Smirnova <tatyanasmirnova404@gmail.com>
2022-09-04 12:38:41 +0300

    Branch junit4 added

--- src/test/java/ru/netology/tests/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/tests/CashbackHackServiceTest.java
@@ -1,44 +1,33 @@
 package ru.netology.tests;
 
-import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
-import ru.netology.service.CashbackHackService;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void testRemain1() {
-
-        assertEquals(service.remain(900),100);
+    public  void testRemain1(){
+        assertEquals(100,service.remain(900));
+    }
+    @Test
+    public  void testRemain2(){
+        assertEquals(1,service.remain(999));
     }
 
     @Test
-    public void testRemain2() {
-
-        assertEquals(service.remain(1000),0);
-
-    }
-    @Test
-    public void testRemain3() {
-
-        assertEquals(service.remain(0),1000);
-
+    public  void testRemain3(){
+        assertEquals(0,service.remain(1000));
     }
 
     @Test
-    public void testRemain4() {
-
-        assertEquals(service.remain(-1),1001);
-
-    }
-    @Test
-    public void testRemain5() {
-
-        assertEquals(service.remain(1),999);
-
+    public  void testRemain4(){
+        assertEquals(1001,service.remain(-1));
     }
 
-}
+
+
+    }
